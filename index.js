b430f26a611522c0db1d02b0433b01ae1246dac7
Rasmus Porsager <rasmus@porsager.com>
2025-09-02 15:25:29 -0600

    Add relative duration
    commit 7d3d9d706a2c94ec57bd03d3ba79886caa7d2287
    Author: Rasmus Porsager <rasmus@porsager.com>
    Date:   Tue Sep 2 15:16:22 2025 -0600
    
        Some good mobbing
    
    commit f982167ce3c43b404badca0733f805f5cd54f776
    Author: Nikolas Savvidis <nicos@gmx.com>
    Date:   Tue Sep 2 19:26:27 2025 +0200
    
        Season of Rasmus
    
    commit 0f1094ecc79a29b20161861e7ad06f8b1b3c2633
    Merge: f01f8c2 9059d59
    Author: Nikolas Savvidis <nicos@gmx.com>
    Date:   Tue Jul 22 22:36:47 2025 +0200
    
        Merge remote-tracking branch 'upstream/master' into next
    
        # Conflicts:
        #	README.md
        #	index.js
    
    commit f01f8c28a9af81f2f9e5aeb04ca907bfbb12599e
    Author: Nikolas Savvidis <nicos@gmx.com>
    Date:   Tue Jul 22 22:21:25 2025 +0200
    
        create a test sample
    
    commit 7a61aa67dbf223cb252db3fcc6ec49cdbe0f5f3b
    Author: Nikolas Savvidis <nicos@gmx.com>
    Date:   Tue Jul 22 22:21:06 2025 +0200
    
        more information
    
    commit 2b0c88e64075c535e3e129d988e4f2424688ec16
    Author: Nikolas Savvidis <nicos@gmx.com>
    Date:   Tue Jul 22 22:19:21 2025 +0200
    
        Support relative fallbacks and seasons
    
    commit 14f6c88778d7c9a18ef2e7d6fcbdf453f3fc0d46
    Author: Nikolas Savvidis <nicos@gmx.com>
    Date:   Tue Jul 22 22:18:53 2025 +0200
    
        Type support
    
    commit db7a3ab7791efde2c612e108356428591898c389
    Author: Nikolas Savvidis <nicos@gmx.com>
    Date:   Mon Jul 21 20:16:17 2025 +0200
    
        use periods instead of uot naming convention
    
    commit cd560ab7e4991f3752b56f23dd3dd7bc90c7fa0f
    Author: Nikolas Savvidis <nicos@gmx.com>
    Date:   Sat Jul 19 03:16:00 2025 +0200
    
        Support relative UOT
    
    commit 2a9cf0ad93af6b91ef97f0f8008c3b074d4d7987
    Author: Nikolas Savvidis <nicos@gmx.com>
    Date:   Sat Jul 19 03:15:12 2025 +0200
    
        Improve readme

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 export default datie
 
+const regexFuture = /^(.*)\s*R(\d+(?:[MHDY])?)?$/
+const regexPast = /^R(\d+(?:[MHDY])?)?\s*(.*)$/
 const cache = new Map()
-const ordinals = ['', 'st', 'nd', 'rd']
-const ordinal = x => ordinals[x] || 'th'
+const ordinals = ['', 'st', 'nd', 'rd', 'th']
+const ordinal = x => ordinals[x] || ordinals[4]
 const sec = [31536000, 2592000, 604800, 86400, 3600, 60, 1]
 
 function format(X) {
-  const weekMS = 1000 * 60 * 60 * 24 * 7
+  const weekMS = 604800000
       , pad = x => x > 9 ? x : '0' + x
 
   const f = {
@@ -47,11 +49,11 @@ function format(X) {
     },
     ww: x => pad(f.w(x)),
     o: (x, p) => ordinal(p % 100 >> 3 ^ 1 && p % 10),
-    Q: x  => Math.floor(x.getMonth()/3) + 1,
-    QQ: x  => pad(f.Q(x)),
-    QQQ: x  => 'Q' + f.Q(x),
-    QQQQ: x  => f.Q(x) + ordinal(f.Q(x)) + ' quarter',
-    R: x => X.relative(x)
+    Q: x => Math.floor(x.getMonth()/3) + 1,
+    QQ: x => pad(f.Q(x)),
+    QQQ: x => 'Q' + f.Q(x),
+    QQQQ: x => f.Q(x) + ordinal(f.Q(x)) + ' quarter',
+    R: (x, b) => parseRelative(x, b)
   }
 
   return f
@@ -60,48 +62,98 @@ function format(X) {
 datie.names = {
   days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
   months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-  periods: ['year', 'month', 'week', 'day', 'hour', 'minute', 'second']
+  periods: ['year', 'month', 'week', 'day', 'hour', 'minute', 'second'],
+  get ordinals() { return ordinals },
+  set ordinals (o) { o.forEach((x, i) => ordinals.splice(i + 1, 1, x)) }
 }
 
 datie.format = format(datie)
 
-function datie(xs) {
-  if (cache.has(xs))
-    return cache.get(xs)
-
-  const f = xs[0]
-      , fns = []
-
+function parseFormat(x, X = datie) {
+  const fns = []
   let l = 0
-  for (let i = 0; i < f.length; i++)
-    f[i] !== f[i + 1] && (fns.push(datie.format[f.slice(l, i + 1)] || f.slice(l, i + 1)), l = i + 1)
+  for (let i = 0; i < x.length; i++)
+    if (x[i] !== x[i + 1])
+      fns.push(X.format[x.slice(l, i + 1)] || x.slice(l, i + 1)), l = i + 1
+  return fns
+}
 
-  const fn = function(x) {
-    x = new Date(x)
-    let last
-    return fns.map(fn => last = typeof fn === 'function' ? fn(x, last) : fn).join('')
+function datie([x]) {
+  if (cache.has(x))
+    return cache.get(x)
+  
+  const R = x.indexOf('R')
+  
+  if (R === -1) {
+    const fns = parseFormat(x)
+    const fn = function (date) {
+      date instanceof Date || (date = new Date(date))
+      let last
+      return fns.map(fn => last = typeof fn === 'function' ? fn(date, last) : fn).join('')
+    }
+    cache.set(x, fn)
+    return fn
   }
+  
+  const past = R === 0
+      , future = !past || x.length === 1
+      , m = past ? x.match(regexPast) : x.match(regexFuture)
+  
+  let threshold = Infinity
 
-  cache.set(xs, fn)
+  const spec = m[past ? 1 : 2]
+      , fallback = m[past ? 2 : 1]
+    
+  if (spec) {
+    let unit = spec[spec.length - 1]
+    const num = isNaN(unit) 
+      ? (unit = spec[spec.length - 1], +spec.slice(0, -1)) 
+      : (unit = 'M', +spec)
+    
+    threshold = num * (
+      unit === 'M' ? sec[5] :
+      unit === 'H' ? sec[4] :
+      unit === 'D' ? sec[3] :
+      unit === 'W' ? sec[2] :
+      sec[0]
+    ) * 1000
+  }
+  
+  const fns = parseFormat(fallback || '')
+
+  const fn = function(date, b = new Date()) {
+    date instanceof Date || (date = new Date(date))
+    b instanceof Date || (b = new Date(b))
+    const diff = b - date
+        , abs = Math.abs(diff)
+    
+    if (past && diff >= 0 && abs <= threshold || future && diff <= 0 && abs <= threshold)
+      return parseRelative(date, b)
+
+    let last
+    return fns.map(fn => last = typeof fn === 'function' ? fn(date, last) : fn).join('')
+  }
+  
+  cache.set(x, fn)
   return fn
 }
 
-datie.relative = function(date, reference = new Date()) {
-
-  let ss = Math.floor((reference - new Date(date)) / 1000);
-  const future = ss < 0;
+function parseRelative(date, reference = Date.now()) {
+  let ss = Math.floor((reference - new Date(date).getTime()) / 1000)
+  const future = ss < 0
   ss = Math.abs(ss)
-
   if (ss < 5)
-    return future ? 'in a few seconds' : 'a few seconds ago'
+    return ss === 0 ? 'now' : ss < 0 ? 'in a few seconds' : 'a few seconds ago'
 
   for (let i = 0, c, u; i < datie.names.periods.length; i++) {
-    c = Math.floor(ss / sec[i]);
+    if (i === 2 && ss > 3 * sec[2])
+      continue
+    c = ~~(ss / sec[i])
     if (c >= 1) {
       u = datie.names.periods[i] + (c !== 1 ? 's' : '')
-      return future ? ('in ' + c + ' ' + u) : (c + ' ' + u + ' ago')
+      return future ? `in ${c} ${u}` : `${c} ${u} ago`
     }
   }
 
-  return 'now';
+  return 'now'
 }
